07a645fd58646990dba3c31668ed5daeaf5e635d
Alex Gilbert <alex.js.ltd@gmail.com>
2022-07-26 20:09:34 +0200

    update

---
+++ src/store/user/user.test.js
@@ -0,0 +1,28 @@
+import { expectSaga } from 'redux-saga-test-plan';
+
+import { fetchUserAsync } from './user.saga';
+import { USER_ACTION_TYPES } from './user.types';
+
+it('just works!', () => {
+  return (
+    expectSaga(fetchUserAsync)
+      // Assert that the `put` will eventually happen.
+      .put({
+        type: USER_ACTION_TYPES.FETCH_USER_SUCCESS,
+        payload: [
+          {
+            email: 'john.doe@email.com',
+            firstName: 'John',
+            lastName: 'Doe',
+            userId: 'rahej',
+          },
+        ],
+      })
+
+      // Dispatch any actions that the saga will `take`.
+      .dispatch({ type: USER_ACTION_TYPES.FETCH_USER_START })
+
+      // Start the test. Returns a Promise.
+      .run()
+  );
+});
